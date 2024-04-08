bc9ad62ed02689a8f21dff9e3868330e4dbd4379
squirt <squirt@pop-os.localdomain>
2024-04-08 11:02:18 -0400

    simple depthwise working

--- simple_nn/main.py
+++ simple_nn/main.py
@@ -4,12 +4,24 @@ from os.path import join
 
 import utils
 
+# Use cuda (else throw error, running on GPU/Grace Hopper)
+assert torch.cuda.is_available(), 'CUDA is not available.'
+
 
 if __name__ == '__main__':
     fname = join('/home/squirt/Documents/data/rp_weather_data/', 'all_data.h5')
+
+    # Going to cross validate (since there is no real test set)
     t_dl, v_dl = utils.get_dataloaders(fname, 256, 0.8)
+
+    # Model 
+    out_dim = int(70*2)
+    model = utils.DepthWiseConv2d(out_dim=out_dim)
 
     for i, (l,x,y) in enumerate(t_dl):
         print(l.shape, x.shape, y.shape)
+
+        x = x.float()
+        print(model(x).shape)
         if i > 5:
             break
--- simple_nn/utils.py
+++ simple_nn/utils.py
@@ -1,10 +1,18 @@
 '''
-Going to write scripts
+Utility file, holds data prep, models, and eval functions.
+Training loop is in main.py
 '''
-
 import torch
 import h5py as h5
 
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+
+#-------------------------
+
+'''
+Data loading and processing functions
+'''
 def load_hdf5(filename:str):
     '''
     Load data from an HDF5 file and return a list of dictionaries.
@@ -59,7 +67,7 @@ def generate_stacks(data:list[dict]) -> tuple[torch.Tensor, torch.Tensor, torch.
     return (landmass, x, y)
 
 
-class weather_dataset(torch.utils.data.Dataset):
+class weather_dataset(Dataset):
     '''
     PyTorch Dataset class for weather data.
     '''
@@ -74,7 +82,7 @@ class weather_dataset(torch.utils.data.Dataset):
         return (self.landmass[idx], self.x[idx], self.y[idx])
 
 
-def get_dataloaders(fname:str, batch_size:int, split:int) -> tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
+def get_dataloaders(fname:str, batch_size:int, split:int) -> tuple[DataLoader, DataLoader]:
     '''
     Create PyTorch DataLoader objects for training and validation data.
     Inputs:
@@ -96,6 +104,50 @@ def get_dataloaders(fname:str, batch_size:int, split:int) -> tuple[torch.utils.d
     train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
 
     # Create DataLoader objects
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True)
     return train_loader, test_loader
+
+
+#-------------------------
+
+'''
+Models
+'''
+
+class DepthWiseConv2d(nn.Module):
+    def __init__(self, out_dim:int, input_shape:list[int]=[70,3,2,2]):
+        super(DepthWiseConv2d, self).__init__()
+        '''
+        Want to convolve over the 2x2 grid of the input values, then apply a pointwise convolution over the features
+        May want to go over again with fresh eyes, Dealing with 5D tensor is a bit confusing
+        Args:
+            - output_dim (int): Number of output features
+            - input_shape (list): Shape of the input tensor, default is [70,3,2,2] since maybe want to change data shape in future???
+        '''
+        # Input: [batch_size, 70*3, 2, 2]
+        self.input_shape = input_shape
+        self.depthwise = nn.Conv2d(
+                            in_channels=self.input_shape[0]*self.input_shape[1], 
+                            out_channels=self.input_shape[0]*self.input_shape[1], 
+                            kernel_size=self.input_shape[2:], 
+                            groups=self.input_shape[0]*self.input_shape[1])
+        self.pointwise = nn.Conv2d(
+                            in_channels=self.input_shape[0]*self.input_shape[1], 
+                            out_channels=self.input_shape[0], 
+                            kernel_size=1)
+        
+        self.out_dim = out_dim
+        self.linear = nn.Linear(input_shape[0], self.input_shape[0]*self.out_dim)
+
+    def forward(self, x):
+        # Do depthwise convolution (no idea if this is a good idea)
+        x = x.view(x.size(0), -1, x.size(3), x.size(4))
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        x = x.squeeze()
+
+        # Want to add an output dim (could handle in larger model, but for now just do it here)
+        x = self.linear(x)
+        x = x.reshape(x.size(0), self.input_shape[0], self.out_dim)
+        return x
