ed3cf49b6129c2b78752c8c4a9f0f4cddcbfae66
squirt <squirt@pop-os.localdomain>
2024-05-30 09:10:42 -0400

    will this preform better

--- transformer_nn/main.py
+++ transformer_nn/main.py
@@ -1,4 +1,9 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.cuda.amp import autocast
+from torch.cuda.amp import GradScaler
 
 from os.path import join
 
@@ -7,21 +12,108 @@ import utils
 # Use cuda (else throw error, running on GPU/Grace Hopper)
 assert torch.cuda.is_available(), 'CUDA is not available.'
 
+'''
+Functions for Training
+'''
+
+def train(model:nn.Module, dl:torch.utils.data.DataLoader, optim:torch.optim, loss:nn.Module) -> float:
+    model.train()
+    total_loss = .0
+    scaler = GradScaler()
+
+    for _, (l, x, y) in enumerate(dl):
+        l = l.cuda()
+        x = x.cuda()
+        y = y.cuda()
+
+        # Combine
+        l = l.unsqueeze(1)
+        x = torch.cat((l, x), 1)
+
+        # Forward pass
+        with autocast():
+            y_pred = model(x)
+            l = loss(y_pred, y)
+            total_loss += l.item()
+
+        # Preform backpass
+        scaler.scale(l).backward()
+        scaler.step(optim)
+        scaler.update()
+    
+    return total_loss / len(dl)
+
+
+def eval(model:nn.Module, dl:torch.utils.data.DataLoader, loss:nn.Module) -> float:
+    model.eval()
+    total_loss = .0
+
+    for _, (l, x, y) in enumerate(dl):
+        l = l.cuda()
+        x = x.cuda()
+        y = y.cuda()
+
+        # Combine
+        l = l.unsqueeze(1)
+        x = torch.cat((l, x), 1)
+
+        # Forward pass
+        with autocast():
+            y_pred = model(x)
+            l = loss(y_pred, y)
+            total_loss += l.item()
+
+    return total_loss / len(dl)
+
+
+def fold(data_file:str, model:nn.Module, i:int) -> tuple[float, float]:
+    '''
+    Do a cross fold training session with early stopping
+    Inputs:
+        - data_file (str): The file to load data from
+        - model (nn.Module): The model to train
+        - i (int): The fold number
+    '''
+    loss_fn = nn.MSELoss()
+    model = model.double().cuda()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    # New Split  
+    t_dl, v_dl = utils.get_dataloaders(data_file, 256, 0.5)
+
+    # Early Stopping 
+    eval_loss = -1*float('inf') 
+    train_loss = 0 
+    eps = 1e-6
+    while (train_loss > eval_loss) and abs(eval_loss - train_loss) > eps:
+        train_loss = train(model, t_dl, optimizer, loss_fn)
+        eval_loss = eval(model, v_dl, loss_fn)
+
+    # Save the model
+    torch.save(model.state_dict(), join('saved_models', f'model_{i}.pt') )
+
+    return (train_loss, eval_loss)
 
 if __name__ == '__main__':
+    # Data 
     fname = join('/home/squirt/Documents/data/weather_data/', 'all_data.h5')
 
-    # Going to cross validate (since there is no real test set)
-    t_dl, v_dl = utils.get_dataloaders(fname, 256, 0.8)
+    # Do cross folds
+    folds = 5 
+    train_loss_rec = []
+    eval_loss_rec = []
+    for i in range(folds):
+        model = utils.ConvTrans(input_shape=[71,3,2,2])
+        train_loss, eval_loss = fold(fname, model, i)
+        train_loss_rec.append(train_loss)
+        eval_loss_rec.append(eval_loss)
+        print(f'Fold {i} Eval Loss: {eval_loss}')
 
-    # Model 
-    out_dim = int(70*2)
-    model = utils.ConvTrans()
+    avg_train_loss = torch.mean(torch.tensor(train_loss_rec)).item()
+    avg_eval_loss = torch.mean(torch.tensor(eval_loss_rec)).item()
 
-    for i, (l,x,y) in enumerate(t_dl):
-        print(l.shape, x.shape, y.shape)
+    std_train_loss = torch.std(torch.tensor(train_loss_rec)).item() 
+    std_eval_loss = torch.std(torch.tensor(eval_loss_rec)).item()
 
-        x = x.float()
-        print(model(x).shape)
-        if i > 5:
-            break
+    print(f'Average Training Loss: {avg_train_loss} +/- {std_train_loss}') 
+    print(f'Average Evaluation Loss: {avg_eval_loss} +/- {std_eval_loss}')
